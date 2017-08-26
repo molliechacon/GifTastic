bfdb56d644e611fd9d0bbaa84609d0ec327250d6
mollie chacon <mollie.mosley@gmail.com>
2017-08-25 21:32:37 -0600

    cleaned up files

--- logic.js
+++ logic.js
@@ -3,57 +3,65 @@ var topics = ["who's the boss", "game of thrones", "playing house", "superstore"
 
 
 for (i = 0; i < topics.length; i++) {
-	$("#tvShowButtons").append('<input type="button" class="btn btn-success" value="' + topics[i] + '">');
-	};
+		$("#tvShowButtons").append('<input type="button" class="btn btn-success" value="' + topics[i] + '">');
+		};
+
 
 $("#addShow").on("click", function(event) {
-  event.preventDefault();
-  var newShow = $("#tvShowInput").val().trim();
-  topics.push(newShow);
-  $("#tvShowButtons").empty();
-  console.log(topics);
-  for(k = 0; k < topics.length; k++) {
-  	$("#tvShowButtons").append('<input type="button" class="btn btn-success" value="' + topics[k] + '">');
-  };
+  		event.preventDefault();
+
+  		var newShow = $("#tvShowInput").val().trim();
+
+  		topics.push(newShow);
+  		$("#tvShowButtons").empty();
+  		console.log(topics);
+  		for(k = 0; k < topics.length; k++) {
+  			$("#tvShowButtons").append('<input type="button" class="btn btn-success" value="' + topics[k] + '">');
+  		}
 })
 
+
 $(".btn").on("click", function() {
-	event.preventDefault();
-  	var show = $(this).attr("value");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      show + "&api_key=dc6zaTOxFJmzC&limit=10";
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).done(function(response) {
-      console.log(response);
-      var results = response.data;
-      for (j = 0; j < results.length; j++) {
-       var showDiv = $("<div>");
-       var showImage = $("<img>");
-       var p = $("<p>").text("Rating: " + results[j].rating);
-       showImage.attr({
-        "src": results[j].images.fixed_height_still.url,
-        "data-still": results[j].images.fixed_height_still.url,
-        "data-animate": results[j].images.fixed_height.url,
-        "data-state": "still",
-        "class": "gif"
-       });
-       showDiv.append(showImage);
-       showDiv.append(p);
-       $("#tvShowGifs").prepend(showDiv);
-	   $(".gif").on("click", function() {
-		var state = $(this).attr("data-state");
-	   	if (state === "still") {
-	    	$(this).attr("src", $(this).attr("data-animate"));
-	        $(this).attr("data-state", "animate");
-	    } else {
-	       	$(this).attr("src", $(this).attr("data-still"));
-	        $(this).attr("data-state", "still");
-	    }
-		}); 
-	  }
-	})
+		event.preventDefault();
+
+  		var show = $(this).attr("value");
+    	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    	$.ajax({
+    		url: queryURL,
+      		method: "GET"
+    	}).done(function(response) {
+     		console.log(response);
+      		var results = response.data;
+      		for (j = 0; j < results.length; j++) {
+
+       			var showDiv = $("<div>");
+       			var showImage = $("<img>");
+       			var p = $("<p>").text("Rating: " + results[j].rating);
+
+		       	showImage.attr({
+			        "src": results[j].images.fixed_height_still.url,
+			        "data-still": results[j].images.fixed_height_still.url,
+			        "data-animate": results[j].images.fixed_height.url,
+			        "data-state": "still",
+			        "class": "gif"
+		       	});
+
+		       	showDiv.append(showImage);
+		       	showDiv.append(p);
+       		   	$("#tvShowGifs").prepend(showDiv);
+	           	$(".gif").on("click", function() {
+		        	var state = $(this).attr("data-state");
+	   				if (state === "still") {
+	    				$(this).attr("src", $(this).attr("data-animate"));
+	        			$(this).attr("data-state", "animate");
+	    			} else {
+	       				$(this).attr("src", $(this).attr("data-still"));
+	        			$(this).attr("data-state", "still");
+	    			}
+				}); 
+	  		}
+		})
 });
 
 
